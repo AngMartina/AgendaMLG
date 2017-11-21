e662804721d4ed7e927984fb64163a70d6f8af6c
Angela <Angela@DESKTOP-MQC7AC2>
2017-11-21 20:33:18 +0100

    Angela -> listadoEventosOrdenacion/eliminacionDeSessionBean 2

--- AgendaMLG-ejb/build/generated-sources/ap-source-output/entity/Usuarios_.java
+++ AgendaMLG-ejb/build/generated-sources/ap-source-output/entity/Usuarios_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-21T20:26:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-21T20:33:04")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
