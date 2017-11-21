5409419e1dcfc1d8853b75671c6074e5f30174f3
Antonio <Antonio@LAPTOP-ANEK5JIT>
2017-11-21 14:12:03 +0100

    Antonio pruebas

--- AgendaMLG-ejb/src/java/ejb/EventoFacade.java
+++ AgendaMLG-ejb/src/java/ejb/EventoFacade.java
@@ -6,9 +6,13 @@
 package ejb;
 
 import entity.Evento;
+import entity.Usuarios;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -27,6 +31,24 @@ public class EventoFacade extends AbstractFacade<Evento> {
 
     public EventoFacade() {
         super(Evento.class);
+    }
+    
+    public List<Evento> buscarEventoPorPreferencias(Usuarios u){
+        Query q;
+        
+        q = em.createQuery("SELECT e FROM Evento WHERE e.palabrasclave LIKE u.preferencias");
+        q.setParameter("u", u);
+        return q.getResultList();
+        
+    }
+    
+    public List<Evento> buscarEventoPorFecha(Date fecha){
+        Query q;
+        
+        q = em.createQuery("SELECT e FROM Evento WHERE fecha BETWEEN e.fechainicio AND e.fechafin");
+        q.setParameter("fecha", fecha);
+        return q.getResultList();
+        
     }
     
 }
