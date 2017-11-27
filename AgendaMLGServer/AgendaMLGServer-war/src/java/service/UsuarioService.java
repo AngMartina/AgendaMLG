243bb6c0824d8db9d061c070b9befb1bfc38c753
Antonio <Antonio@LAPTOP-ANEK5JIT>
2017-11-27 19:17:32 +0100

    Antonio 27/11/17

--- AgendaMLGServer/AgendaMLGServer-war/src/java/service/UsuarioService.java
+++ AgendaMLGServer/AgendaMLGServer-war/src/java/service/UsuarioService.java
@@ -10,12 +10,18 @@ import ejb.EventoFacade;
 import ejb.UsuariosFacade;
 import entity.Evento;
 import entity.Usuarios;
+import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
  *
@@ -32,107 +38,125 @@ public class UsuarioService {
 
     @EJB
     private EtiquetasFacade etiquetasFacade;
-    
-
 
     /**
      * Web service operation
+     *
      * @param autenticacionEmailIntroducido
      * @param usuario
      * @param mensajeError
-     * @return 
+     * @return
      */
     @WebMethod(operationName = "iniciarSesion")
-     public Usuarios iniciarSesion(String autenticacionEmailIntroducido, Usuarios usuario, String mensajeError) {
+    public Usuarios iniciarSesion(String autenticacionEmailIntroducido, Usuarios usuario, String mensajeError) {
         if (!autenticacionEmailIntroducido.isEmpty() && autenticacionEmailIntroducido != null) {
             usuario = usuariosFacade.findByEmail(autenticacionEmailIntroducido);
-        }else{
-            usuario =null;
+        } else {
+            usuario = null;
         }
         return usuario;
     }
-     
-     /**
+
+    /**
      * Web service operation
-     * @return 
+     *
+     * @return
      */
     @WebMethod(operationName = "obtenerUsuarios")
-     public List<Usuarios> obtenerUsuarios(){
-            return usuariosFacade.findAll();
-        
+    public List<Usuarios> obtenerUsuarios() {
+        return usuariosFacade.findAll();
+
     }
-    
-      /**
+
+    /**
      * Web service operation
+     *
      * @param ordenacion
      * @param usuario
      * @param fechaOrdenacion
      * @param localizacionLatitud
      * @param localizacionLongitud
-     * @return 
+     * @return
+     * @throws javax.xml.datatype.DatatypeConfigurationException
      */
     @WebMethod(operationName = "obtenerEventos")
-    public List<Evento> obtenerEventos(Integer ordenacion, Usuarios usuario, Date fechaOrdenacion, double localizacionLatitud, double localizacionLongitud){
-       if(null==ordenacion){
-           return eventoFacade.findAll();
-       } else switch (ordenacion) {
-            case 1:
-                //Ordeno por preferencias
-                return eventoFacade.buscarEventoPorPreferencias(usuario);
-            case 2:
-                //Ordeno por fecha
-                return eventoFacade.buscarEventoPorFecha(fechaOrdenacion);
-            case 3:
-                //Ordeno por distancia
-                double a = localizacionLatitud;
-                double b = localizacionLongitud;
-                return null; //FALTA POR HACER
-            default:
+    public List<Evento> obtenerEventos(Integer ordenacion, Usuarios usuario, Date fechaOrdenacion, double localizacionLatitud, double localizacionLongitud) throws DatatypeConfigurationException {
+        if (null == ordenacion) {
+            //if (usuario.getTipoUsuario() == 3) { // Periodista
                 return eventoFacade.findAll();
+            //} else {
+            //    GregorianCalendar fechaActual = new GregorianCalendar();
+            //    return eventoFacade.EventosNoCaducados(fechaActual);
+                 
+            //}
+        } else {
+            switch (ordenacion) {
+                case 1:
+                    //Ordeno por preferencias
+                    return eventoFacade.buscarEventoPorPreferencias(usuario);
+                case 2:
+                    //Ordeno por fecha
+                   return eventoFacade.buscarEventoPorFecha(fechaOrdenacion);
+                case 3:
+                    //Ordeno por distancia
+                    double a = localizacionLatitud;
+                    double b = localizacionLongitud;
+                    return null; //FALTA POR HACER
+                case 4:
+                    return eventoFacade.findAll();
+                default:
+                    return eventoFacade.findAll();
+            }
         }
     }
-    
+
     /**
-    * Web service operation
-    * @return 
-    */
-    @WebMethod(operationName = "subirEvento")
-   public String subirEvento(){
-       return "crearEvento";
-   }
-   
-   
-   
-   
-        /**
      * Web service operation
+     *
+     * @return
+     */
+    @WebMethod(operationName = "subirEvento")
+    public String subirEvento() {
+        return "crearEvento";
+    }
+
+    /**
+     * Web service operation
+     *
      * @param nuevoEvento
      * @param usuario
-     * @return 
+     * @param fechainicio
+     * @param fechafin
+     * @return
      */
     @WebMethod(operationName = "crearEvento")
-    public String crearEvento(Evento nuevoEvento, Usuarios usuario){
-        /*Codigo Antonio: Me falta enlazarlo con un usuario*/
-        if(usuario.getTipoUsuario() != 3){//Usuario normal y Superusuario
-            nuevoEvento.setEstado(0); //Sin validar   
-        }else{//Periodista
-            nuevoEvento.setEstado(1); //Validado
+    public String crearEvento(Evento nuevoEvento, Usuarios usuario, Date fechainicio, Date fechafin) {
+        if (usuario.getTipoUsuario() != 1) {//Usuario normal y Superusuario
+            nuevoEvento.setEstado(1); //Validado   
+        } else {//Periodista
+            nuevoEvento.setEstado(0);//Sin validar 
         }
-        nuevoEvento.setEmailusuario(usuario); //No me deja hacer getEmail(), por?
+        nuevoEvento.setFechainicio(fechainicio);
+        nuevoEvento.setFechafin(fechafin);
+        nuevoEvento.setEmailusuario(usuario); //No me deja hacer getEmail()
         this.eventoFacade.create(nuevoEvento);
-        return "listaEventos";
+        return "/eventos/listaEventos?faces-redirect=true";
     }
-    
-    
+
+    public Date formateoFecha(XMLGregorianCalendar fechaAParsear) {
+        return fechaAParsear.toGregorianCalendar().getTime();
+
+    }
+
     /**
      * Web service operation
+     *
      * @param usuarioLoggeado
      * @return
      */
     @WebMethod(operationName = "obtenerEventosDeUsuario")
-    public List<Evento> obtenerEventosDeUsuario(Usuarios usuarioLoggeado){
+    public List<Evento> obtenerEventosDeUsuario(Usuarios usuarioLoggeado) {
         return this.eventoFacade.EventosDeUsuario(usuarioLoggeado);
     }
 
-    
 }
